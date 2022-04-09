244dd120898426db80c047376a5f1d6d6ca2d854
Tsubasa Tanabe <aburasoba.blog@gmail.com>
2022-04-09 16:28:34 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,29 +1,152 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
+import "./App.css";
+/* ethers 変数を使えるようにする*/
 import { ethers } from "ethers";
-import './App.css';
+/* ABIファイルを含むWavePortal.jsonファイルをインポートする*/
+import abi from "./utils/WavePortal.json";
+const App = () => {
+  /*
+   * ユーザーのパブリックウォレットを保存するために使用する状態変数を定義します。
+   */
+  const [currentAccount, setCurrentAccount] = useState("");
+  console.log("currentAccount: ", currentAccount);
+  /**
+   * デプロイされたコントラクトのアドレスを保持する変数を作成
+   */
+  const contractAddress = "0x42Ff30c9Fc4C1901f199D580daE8A88f00312A0E";
+  /**
+   * ABIの内容を参照する変数を作成
+   */
+  const contractABI = abi.abi;
 
-export default function App() {
+  /*
+   * window.ethereumにアクセスできることを確認します。
+   */
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        console.log("Make sure you have MetaMask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+      /*
+       * ユーザーのウォレットへのアクセスが許可されているかどうかを確認します。
+       */
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account);
+      } else {
+        console.log("No authorized account found");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  /*
+   * connectWalletメソッドを実装
+   */
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      console.log("Connected: ", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  /*
+   * waveの回数をカウントする関数を実装
+   */
+  const wave = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        /*
+         * ABIを参照
+         */
+        const wavePortalContract = new ethers.Contract(
+            contractAddress,
+            contractABI,
+            signer
+        );
+        let count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+        /*
+         * コントラクトに👋（wave）を書き込む。
+         */
+        const waveTxn = await wavePortalContract.wave();
+        console.log("Mining...", waveTxn.hash);
+        await waveTxn.wait();
+        console.log("Mined -- ", waveTxn.hash);
+        count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  const wave = () => {
-
-  }
-
+  /*
+   * WEBページがロードされたときに下記の関数を実行します。
+   */
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
   return (
-    <div className="mainContainer">
-
-      <div className="dataContainer">
-        <div className="header">
-        <span role="img" aria-label="hand-wave">👋</span> WELCOME!
+      <div className="mainContainer">
+        <div className="dataContainer">
+          <div className="header">
+          <span role="img" aria-label="hand-wave">
+            👋
+          </span>{" "}
+            WELCOME!
+          </div>
+          <div className="bio">
+            イーサリアムウォレットを接続して、「
+            <span role="img" aria-label="hand-wave">
+            👋
+          </span>
+            (wave)」を送ってください
+            <span role="img" aria-label="shine">
+            ✨
+          </span>
+          </div>
+          {/*
+         * waveボタンにwave関数を連動させる。
+         */}
+          <button className="waveButton" onClick={wave}>
+            Wave at Me
+          </button>
+          {/*
+         * ウォレットコネクトのボタンを実装
+         */}
+          {!currentAccount && (
+              <button className="waveButton" onClick={connectWallet}>
+                Connect Wallet
+              </button>
+          )}
+          {currentAccount && (
+              <button className="waveButton" onClick={connectWallet}>
+                Wallet Connected
+              </button>
+          )}
         </div>
-
-        <div className="bio">
-        イーサリアムウォレットを接続して、メッセージを作成したら、<span role="img" aria-label="hand-wave">👋</span>を送ってください<span role="img" aria-label="shine">✨</span>
-        </div>
-
-        <button className="waveButton" onClick={wave}>
-        Wave at Me
-        </button>
       </div>
-    </div>
   );
-}
+};
+export default App;
